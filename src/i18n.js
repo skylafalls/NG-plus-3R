804c80fe167bc20c08f071284f620f589986991c
HexaVault <44529573+HexaVault@users.noreply.github.com>
2026-03-24 00:27:19 +0000

    push so i can start crawling back through commits to find number problem

--- src/i18n.js
+++ src/i18n.js
@@ -1,6 +1,100 @@
 /* eslint-disable no-negated-condition */
 import * as i18nText from "./i18n/exports";
 
+window.i18n = function(type, id, mods = []) {
+  let text = "";
+  // eslint-disable-next-line import/namespace
+  // If the player is holding the "show formula" keybind, use the formula i18n
+  if (Lang.showFormula) {
+    text = Lang.current.allText[type][id];
+  }
+
+  // Else go to language
+  if (text === undefined || text === "") {
+    text = Lang.current.allText[type][id];
+  }
+
+  // If it's not defined for that language, default to English
+  if (text === undefined || text === "") {
+    text = Lang.GBR_EN.allText[type][id];
+  }
+
+  // If it's not defined for English, default to "Placeholder"
+  if (text === undefined || text === "") {
+    text = "Placeholder";
+  }
+
+  // We do this first, since $1aX$KEY$$ needs to be handled before it becomes VALUE$KEY$$ and breaks
+  // Note if a key:name is not defined, it just wont get substituted here, but that only happens if bad translating anyways
+  // (You would have to call a pluralisation and then not define it for it to occur, but plurals can just not be used)
+  const plurals = Lang.current.allText.plurals;
+  for (const value in plurals) {
+    const key = Lang.current.allText.plurals[value];
+    text = text.replaceAll(`/\$([0-9]{1,2}aX)?\$${key.key}\$\$/g`, match => pluralHandling(match, key.rules, mods));
+  }
+
+  for (let i = 1; i <= mods.length; i++) {
+    text = text.replaceAll(`$${i}aX`, handlePossibleFunction(mods[i - 1]));
+  }
+  return text;
+};
+
+class LanguageState {
+  constructor(allText) {
+    this.allText = allText;
+  }
+
+  get name() {
+    return player.options.englishLangNames ? this.nameInEN : this.nameInLang;
+  }
+
+  get nameInLang() {
+    return this.allText.options.name;
+  }
+
+  get nameInEN() {
+    return this.allText.options.nameInEN;
+  }
+
+  get shortName() {
+    return this.allText.shortName;
+  }
+
+  setAsCurrent(silent) {
+    player.options.language = this.shortName;
+    // TODO: This should be i18n, how did we miss this
+    if (!silent) GameUI.notify.success(`Set language to ${this.name}`);
+  }
+}
+
+// Local version of "Map Game Date to Object" function in game database
+function mgdtoLocal(gameData, mapFun) {
+  const array = Object.entries(gameData);
+  const out = {};
+  for (let idx = 0; idx < array.length; idx++) {
+    out[array[idx][0]] = mapFun(array[idx][1]);
+  }
+  return {
+    all: Object.values(out),
+    ...out
+  };
+};
+
+export const Lang = mgdtoLocal(
+  // Weird code to add the short name of the language (ie. EN) to all text
+  Object.fromEntries(Object.entries(i18nText).map(i => [i[0], { ...i[1], shortName: i[0] }])),
+  allText => new LanguageState(allText)
+);
+
+Object.defineProperty(Lang, "current", {
+  get() { return Lang[typeof player !== "undefined" ? player.options.language : "GBR_EN"]; }
+});
+
+Object.defineProperty(Lang, "showFormula", {
+  get() { return typeof player !== "undefined" ? player.options.showFomula : false; },
+  set(value) { player.options.showFomula = value; }
+});
+
 // eslint-disable-next-line max-params
 function pluralHandling(textInput, rules, mods) {
   // We dont wan't to modify inputs, so we'll redefine here.
@@ -66,97 +160,3 @@ function pluralHandling(textInput, rules, mods) {
 
   return rules[-1];
 }
-
-window.i18n = function(type, id, mods = []) {
-  let text = "";
-  // eslint-disable-next-line import/namespace
-  // If the player is holding the "show formula" keybind, use the formula i18n
-  if (Lang.showFormula) {
-    text = Lang.current.allText[type][id];
-  }
-
-  // Else go to language
-  if (text === undefined || text === "") {
-    text = Lang.current.allText[type][id];
-  }
-
-  // If it's not defined for that language, default to English
-  if (text === undefined || text === "") {
-    text = Lang.GBR_EN.allText[type][id];
-  }
-
-  // If it's not defined for English, default to "Placeholder"
-  if (text === undefined || text === "") {
-    text = "Placeholder";
-  }
-
-  // We do this first, since $1aX$KEY$$ needs to be handled before it becomes VALUE$KEY$$ and breaks
-  // Note if a key:name is not defined, it just wont get substituted here, but that only happens if bad translating anyways
-  // (You would have to call a pluralisation and then not define it for it to occur, but plurals can just not be used)
-  plurals = Lang.plurals;
-  for (value in plurals) {
-    key = Lang.plurals[value];
-    text = text.replaceAll(`/\$([0-9]{1,2}aX)?\$${key.key}\$\$/g`, match => pluralHandling(match, key.rules, mods));
-  }
-
-  for (let i = 1; i <= mods.length; i++) {
-    text = text.replaceAll(`$${i}aX`, handlePossibleFunction(mods[i - 1]));
-  }
-  return text;
-};
-
-class LanguageState {
-  constructor(allText) {
-    this.allText = allText;
-  }
-
-  get name() {
-    return player.options.englishLangNames ? this.nameInEN : this.nameInLang;
-  }
-
-  get nameInLang() {
-    return this.allText.options.name;
-  }
-
-  get nameInEN() {
-    return this.allText.options.nameInEN;
-  }
-
-  get shortName() {
-    return this.allText.shortName;
-  }
-
-  setAsCurrent(silent) {
-    player.options.language = this.shortName;
-    // TODO: This should be i18n, how did we miss this
-    if (!silent) GameUI.notify.success(`Set language to ${this.name}`);
-  }
-}
-
-// Local version of "Map Game Date to Object" function in game database
-function mgdtoLocal(gameData, mapFun) {
-  const array = Object.entries(gameData);
-  const out = {};
-  for (let idx = 0; idx < array.length; idx++) {
-    out[array[idx][0]] = mapFun(array[idx][1]);
-  }
-  return {
-    all: Object.values(out),
-    ...out
-  };
-};
-
-export const Lang = mgdtoLocal(
-  // Weird code to add the short name of the language (ie. EN) to all text
-  Object.fromEntries(Object.entries(i18nText).map(i => [i[0], { ...i[1], shortName: i[0] }])),
-  allText => new LanguageState(allText)
-);
-
-Object.defineProperty(Lang, "current", {
-  get() { return Lang[typeof player !== "undefined" ? player.options.language : "GBR_EN"]; }
-});
-
-Object.defineProperty(Lang, "showFormula", {
-  get() { return typeof player !== "undefined" ? player.options.showFomula : false; },
-  set(value) { player.options.showFomula = value; }
-});
