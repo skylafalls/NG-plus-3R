29104f71f082048aafe641cdd22d634a43fc061c
Skylafalls <triplettran56@gmail.com>
2026-04-09 09:40:54 -0400

    adkv
    Signed-off-by: Skylafalls <triplettran56@gmail.com>

--- eslint.config.js
+++ eslint.config.js
@@ -1,18 +1,13 @@
 // @ts-check
 import oxlint from "eslint-plugin-oxlint";
-import stylistic from "@stylistic/eslint-plugin";
 import tseslint from "typescript-eslint";
 import vue from "eslint-plugin-vue";
 
 const config = tseslint.config(
   tseslint.configs.strict,
   ...vue.configs["flat/vue2-recommended"],
-  stylistic.configs.recommended,
   {
     rules: {
-      "@stylistic/quotes": ["error", "double"],
-      "@stylistic/semi": ["error", "always"],
-      "@stylistic/brace-style": ["error", "1tbs"],
       "@typescript-eslint/no-explicit-any": "off",
     },
   },
