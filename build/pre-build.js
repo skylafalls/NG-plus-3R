b7ac0cc338786a97a452ad10be3f338015ff4d6a
Skylafalls <triplettran56@gmail.com>
2026-04-13 13:23:52 -0400

    update deps, fix build scripts
    Signed-off-by: Skylafalls <triplettran56@gmail.com>

--- build/pre-build.js
+++ build/pre-build.js
@@ -1,8 +1,8 @@
 import fs from "fs";
 import path from "path";
 
-import browserslist from "browserslist-useragent-regexp";
+import { getUserAgentRegex } from "browserslist-useragent-regexp";
 
-const userAgentRegExp = browserslist.getUserAgentRegExp({ allowHigherVersions: true });
+const userAgentRegExp = getUserAgentRegex({ allowHigherVersions: true });
 const checkFunction = `export const supportedBrowsers = ${userAgentRegExp};`;
 fs.writeFileSync(path.resolve(import.meta.dirname, "../src/supported-browsers.js"), checkFunction);
